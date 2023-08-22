4ed11cd464a212f35bb566e925492f1ab740937a
zahraabs <zahraabbasiun@gmail.com>
2023-08-22 11:42:13 +0330

    first page javascript code for sound button and buttons uppercase added

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+// make variables
+let homeButtons = document.querySelectorAll(".HomePage__Links")
+const sound = document.querySelector(".HomePage__Sound");
+
+let soundOnOff = true;
+
+
+const tickAudio = new Audio("./assets/sounds/Check mark sound effect.mp3")
+
+homeButtons.forEach(item => {
+    item.innerText = item.innerText.toUpperCase();
+})
+
+sound.addEventListener("click", e => {
+    if (soundOnOff) {
+        e.target.src = "./assets/images/soundOff.png";
+        soundOnOff = false;
+    } else if (soundOnOff == false) {
+        e.target.src = "./assets/images/sound.png";
+        soundOnOff = true;
+        tickAudio.play()
+    }
+})

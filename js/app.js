0e96bf1bff12c25ba82630a53798edeefd0cfa68
zahraabs <zahraabbasiun@gmail.com>
2023-09-15 15:26:31 +0330

    refactore app.js script and add hide and showElement functions

--- js/app.js
+++ js/app.js
@@ -1,25 +1,40 @@
 "use strict";
-// Declare variables
+
+// DOM Selectors
 const homeButtons = document.querySelectorAll(".homeItems__links");
 const sound = document.querySelector(".animationFrame__sound");
-
-// Modal Selectors
 const modal = document.querySelector(".modal");
 const openModalBtn = document.querySelector(".questionIcon");
 const closeModalBtn = document.querySelector(".btn-close");
-
 const easyLevelButton = document.getElementById("easy-level");
 const mediumLevelButton = document.getElementById("medium-level");
 const hardLevelButton = document.getElementById("hard-level");
 const playButton = document.querySelector(".buttonPlay");
+const mainPage = document.querySelector(".main-page");
+const singlePlayerPage = document.querySelector(".single-player-page");
 
+// Variables
 let soundOn = false;
-let selectedLevel =null;
-
+let selectedLevel = null;
 const backgroundMusic = new Audio("./assets/sounds/Freedom.mp3");
 
-// Play audio when sound icon clicked
-sound.addEventListener("click", () => {
+// Event Listeners
+sound.addEventListener("click", toggleSound);
+homeButtons.forEach((item) => {
+	item.innerText = item.innerText.toUpperCase();
+});
+document.querySelector(".homeIcon").addEventListener("click", backToHome);
+document.querySelector(".btn-single-player").addEventListener("click", switchToSinglePlayer);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+easyLevelButton.addEventListener("click", () => selectLevel("easy"));
+mediumLevelButton.addEventListener("click", () => selectLevel("medium"));
+hardLevelButton.addEventListener("click", () => selectLevel("hard"));
+playButton.addEventListener("click", playGame);
+
+// Functions
+function toggleSound() {
 	if (!soundOn) {
 		sound.src = "./assets/images/sound.png";
 		soundOn = true;
@@ -29,11 +44,7 @@ sound.addEventListener("click", () => {
 		soundOn = false;
 		pauseBackgroundMusic();
 	}
-});
-
-homeButtons.forEach((item) => {
-	item.innerText = item.innerText.toUpperCase();
-});
+}
 
 function playBackgroundMusic() {
 	backgroundMusic.muted = false;
@@ -44,83 +55,72 @@ function pauseBackgroundMusic() {
 	backgroundMusic.pause();
 }
 
-// Switch to single player
-const switchToSinglePlayer = () => {
-	const mainPage = document.querySelector(".main-page");
-	const singlePlayerPage = document.querySelector(".single-player-page");
-	mainPage.style.display = "none";
-	singlePlayerPage.style.display = "flex";
-};
-
-const backToHome = () => {
-	const mainPage = document.querySelector(".main-page");
-	const singlePlayerPage = document.querySelector(".single-player-page");
-	selectedLevel = null;
-	mainPage.style.display = "flex";
-	singlePlayerPage.style.display = "none";
-	removeClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-};
-
-document.querySelector(".homeIcon").addEventListener("click", backToHome);
-document
-	.querySelector(".btn-single-player")
-	.addEventListener("click", switchToSinglePlayer);
-
-openModalBtn.addEventListener("click", () => {
-	modal.classList.remove("hidden");
-});
-
-closeModalBtn.addEventListener("click", () => {
-	modal.classList.add("hidden");
-});
-
-modal.addEventListener("click", (e) => {
-	if (e.target === modal) {
-		modal.classList.add("hidden");
-	}
-});
-
-// Play game based on selected level
-easyLevelButton.addEventListener("click", function () {
-	selectedLevel = "easy";
-	toggleClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active")
-	removeClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-});
-
-mediumLevelButton.addEventListener("click", function () {
-	selectedLevel = "medium";
-	toggleClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active")
-	removeClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-});
-
-hardLevelButton.addEventListener("click", function () {
-	selectedLevel = "hard";
-	toggleClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-	removeClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active");
-});
-
-playButton.addEventListener("click", function () {
-	// Show the page for the selected level.
-	if (selectedLevel === "easy") {
-		window.location.href = "./quickPlay.html?level=easy";
-	} else if (selectedLevel === "medium") {
-		window.location.href = "./quickPlay.html?level=medium";
-	} else if (selectedLevel === "hard") {
-		window.location.href = "./quickPlay.html?level=hard";
-	}
-});
-
-function removeClass(level, className, linkClassName) {
-	level.classList.remove(`${className}`);
-	level.children[0].classList.remove(`${linkClassName}`);
+function switchToSinglePlayer() {
+	hideElement(mainPage);
+	showElement(singlePlayerPage);
 }
 
-function toggleClass(level, className, linkClassName) {
-	level.classList.toggle(`${className}`);
-	level.children[0].classList.toggle(`${linkClassName}`);
+function backToHome() {
+	selectedLevel = null;
+	removeClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	removeClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	removeClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	hideElement(singlePlayerPage);
+	showElement(mainPage);
+}
+
+function openModal() {
+	removeClass(modal, "hidden");
+}
+
+function closeModal(e) {
+	if (e.target === closeModalBtn || e.target === modal) {
+	  modal.classList.add("hidden");
+	}
+  }
+
+function selectLevel(level) {
+	selectedLevel = level;
+	toggleClass(
+		level === "easy" ? easyLevelButton : level === "medium" ? mediumLevelButton : hardLevelButton,
+		"homeItems__buttons-active",
+		"homeItems__links-active"
+	);
+	
+	// Remove active class from the other level buttons
+	if (level !== "easy") {
+		removeClass(easyLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	}
+
+	if (level !== "medium") {
+		removeClass(mediumLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	}
+
+	if (level !== "hard") {
+		removeClass(hardLevelButton, "homeItems__buttons-active", "homeItems__links-active");
+	}
+}
+
+function playGame() {
+	if (selectedLevel) {
+		window.location.href = `./quickPlay.html?level=${selectedLevel}`;
+	}
+}
+
+function removeClass(element, className, linkClassName) {
+	element.classList.remove(className);
+	element.children[0].classList.remove(linkClassName);
+}
+
+function toggleClass(element, className, linkClassName) {
+	element.classList.toggle(className);
+	element.children[0].classList.toggle(linkClassName);
+}
+
+function hideElement(element) {
+	element.style.display = "none";
+}
+
+function showElement(element) {
+	element.style.display = "flex";
 }
